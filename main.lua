284c76c7b10769b39332cc9fe5c56615ed44d7ec
hesam <hesaam@riseup.net>
2020-04-12 22:49:10 +0430

    integrate with love engine

---
+++ main.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua5.3
+
+-- Canvas width and height
+local cw = 380
+local ch = 640
+
+function love.load()
+    love.window.setTitle("DicE")
+    love.window.setMode(cw,ch,{resizable=false, vsync=false})
+    
+
+end
+ 
+ 
+
+
+function love.draw()
+
+end
+
+
+
+
