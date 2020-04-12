c8bdf204236851daf82e90071ca361121fdcfc65
hesam <hesaam@riseup.net>
2020-04-12 23:30:55 +0430

    resources + changes in love.load

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
 #!/usr/bin/env lua5.3
 
--- Canvas width and height
+-- screen size settings
 local cw = 380
 local ch = 640
 
+
+--create tables 
+local dave = {}
+local hal = {}
+
+-- set colors for players
+dave.color = {220/255, 220/255, 220/255} 
+dave.color = {52/255, 52/255, 133/255} 
+
 function love.load()
     love.window.setTitle("DicE")
-    love.window.setMode(cw,ch,{resizable=false, vsync=false})
-    
+    love.window.setMode(cw, ch, {resizable=false, vsync=false})
+    love.graphics.setBackgroundColor(25/255, 125/255, 75/255)
 
 end
  
